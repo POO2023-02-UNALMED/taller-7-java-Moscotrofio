27b5c6744d20a913c5a8632501bf9eaa3e433eeb
Moscotrofio <indarttomas@gmail.com>
2023-10-05 21:24:08 -0500

    Update Alfabeto.java

--- src/main/java/comunicacion/Alfabeto.java
+++ src/main/java/comunicacion/Alfabeto.java
@@ -3,8 +3,10 @@ public class Alfabeto extends Pictograma {
     private String[] letras;
     private String interpretacion;
 
-    public Alfabeto(String origen) {
+    public Alfabeto(String origen, String[] letras, String interpretacion) {
         super(origen);
+        this.letras = letras;
+        this.interpretacion = interpretacion;
     }
 
     public int cantidadLetras() {
